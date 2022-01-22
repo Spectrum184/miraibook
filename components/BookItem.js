b9d851c6875d046bb74da7526f6e25fa84b77a18
spectrum184 <nvthanh1804@gmail.com>
2022-01-22 22:28:04 +0900

    fix: margin book item

--- components/BookItem.js
+++ components/BookItem.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const BookItem = ({ image, name }) => {
   return (
-    <div className="flex flex-col w-full px-2 md:px-4 lg:px-6 md:mb-4 md:w-1/2 lg:w-1/4">
+    <div className="flex flex-col w-full items-center px-2 my-4 md:my-0 md:px-4 lg:px-6 md:mb-4 md:w-1/2 lg:w-1/4">
       <span className="relative w-full h-64 rounded-sm overflow-hidden cursor-pointer">
         <Image
           src={image}

f5297a785b23d36cc489cc61167e7b52881be067
spectrum184 <nvthanh1804@gmail.com>
2022-01-24 00:11:24 +0900

    fix: css navbar don't fixed on mobile and tablet

--- components/Navbar.js
+++ components/Navbar.js
@@ -40,17 +40,20 @@ const Navbar = () => {
       <HeaderTop headerRef={headerRef} />
       {/* never mind, add one div for fixed height of navbar menu, i can't set sticky */}
       {onScroll && (
-        <div style={{ height: headerMenuRef.current?.clientHeight }}></div>
+        <div
+          className="hidden lg:block"
+          style={{ height: headerMenuRef.current?.clientHeight }}
+        ></div>
       )}
       <div
         ref={headerMenuRef}
         className={clsx('w-full bg-white z-50', {
-          'shadow-md fixed top-0 transition ease-in-out duration-300': onScroll,
+          'lg:shadow-md lg:fixed lg:top-0': onScroll,
         })}
       >
-        <div className="flex content-center relative justify-between max-w-screen-xl mx-auto lg:px-6">
+        <div className="flex content-center justify-between max-w-screen-xl flex-wrap lg:flex-nowrap lg:flex-row-reverse mx-auto lg:px-6">
           <span
-            className="px-4 py-4 lg:hidden"
+            className="px-4 py-4 lg:hidden w-1/2"
             onClick={() => setIsOpen(!isOpen)}
           >
             {isOpen ? (
@@ -85,9 +88,49 @@ const Navbar = () => {
               </svg>
             )}
           </span>
+          <div className="w-1/2 my-auto lg:w-fit">
+            <div className="float-right relative pr-4 flex items-center">
+              {onSearch && (
+                <input
+                  type="search"
+                  className="px-4 py-1 w-32 text-gray-800 rounded-full border border-gray-400 outline-none mr-3"
+                  placeholder="search"
+                />
+              )}
+              <svg
+                onClick={() => setOnSearch(!onSearch)}
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-3 hover:text-pink-500 cursor-pointer text-orange-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 hover:text-pink-500 cursor-pointer text-orange-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                />
+              </svg>
+            </div>
+          </div>
           <ul
             className={clsx(
-              'absolute top-14 z-50 w-full px-4 flex-col border-b-2 border-gray-300 lg:border-b-0 lg:flex lg:flex-row lg:items-center lg:relative lg:top-0 lg:px-0 bg-white',
+              'z-50 w-full px-4 flex-col border-b-2 border-gray-300 lg:border-b-0 lg:flex lg:flex-row lg:items-center lg:relative lg:top-0 lg:px-0 bg-white',
               {
                 flex: isOpen,
                 hidden: !isOpen,
@@ -119,45 +162,6 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-
-          <div className="flex items-center pr-4 relative">
-            {onSearch && (
-              <input
-                type="search"
-                className="px-4 py-1 w-32 text-gray-800 rounded-full border border-gray-400 outline-none mr-3"
-                placeholder="search"
-              />
-            )}
-            <svg
-              onClick={() => setOnSearch(!onSearch)}
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 mr-3 hover:text-pink-500 cursor-pointer text-orange-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 hover:text-pink-500 cursor-pointer text-orange-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-              />
-            </svg>
-          </div>
         </div>
         <div
           className={clsx(
